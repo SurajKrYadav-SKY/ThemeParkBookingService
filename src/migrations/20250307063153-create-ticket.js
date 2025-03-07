4b5476a9995f3b63d01233b571a66a4da201ef8a
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2025-03-07 12:07:11 +0530

    created the ticket model using sequelize

---
+++ src/migrations/20250307063153-create-ticket.js
@@ -0,0 +1,41 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Tickets", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      park_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      booking_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      visit_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Tickets");
+  },
+};
---
+++ src/models/ticket.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Ticket extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Ticket.init(
+    {
+      user_id: { type: DataTypes.INTEGER, allowNull: false },
+      park_id: { type: DataTypes.INTEGER, allowNull: false },
+      booking_date: { type: DataTypes.DATE, allowNull: false },
+      visit_date: { type: DataTypes.DATE, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "Ticket",
+    }
+  );
+  return Ticket;
+};
