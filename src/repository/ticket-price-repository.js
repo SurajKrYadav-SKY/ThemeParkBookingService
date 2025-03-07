00235082835e2cb397b266a183cd73a1cf7b5cbf
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2025-03-07 16:36:05 +0530

    added ticket price repository

---
+++ src/repository/ticket-price-repository.js
@@ -0,0 +1,17 @@
+const TicketPrice = require("../models/index");
+
+class TicketPriceRepository {
+  async getPricesByParkId(park_id) {
+    try {
+      const pricesByPark = await TicketPrice.findOne({
+        where: { park_id },
+      });
+      return pricesByPark;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+}
+
+module.exports = new TicketPriceRepository();
