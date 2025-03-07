188e1e8ea3c867740df8f09cb8f4c45efcea1fec
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2025-03-07 16:14:50 +0530

    implemented the routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
 
 const setupAndStartServer = () => {
   const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
 
   app.listen(PORT, async () => {
     console.log(`server started at port: ${PORT}`);
