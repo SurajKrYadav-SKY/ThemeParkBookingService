cb30644043863f3b7073eb1d3e1b3cadb9f150c3
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2025-03-07 16:31:15 +0530

    added the booking repository

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,43 @@
+const { where } = require("sequelize");
+const Booking = require("../models/booking");
+
+class BookingRepository {
+  async createBooking(data) {
+    try {
+      const booking = await Booking.create(data);
+      return booking;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async findBookingById(id) {
+    try {
+      const booking = await Booking.findByPk(id);
+      return booking;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async getBookingsForDate(pard_id, visit_date) {
+    try {
+      const bookingForDate = await Booking.findAll({
+        include: [
+          {
+            model: Ticket,
+            where: { pard_id, visit_date },
+          },
+        ],
+      });
+      return bookingForDate;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+}
+
+module.exports = BookingRepository;
