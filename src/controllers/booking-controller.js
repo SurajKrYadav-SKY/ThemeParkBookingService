f6811d97d10323483799340320c34ea85a0b91a7
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2025-03-07 19:22:31 +0530

    added the createBooking controller

---
+++ src/controllers/booking-controller.js
@@ -0,0 +1,31 @@
+const BookingService = require("../services/booking-service");
+
+const bookingService = new BookingService();
+
+const createBooking = async (req, res) => {
+  try {
+    const { user_id, park_id, visit_date, passengers } = req.body;
+    const response = await bookingService.createBooking({
+      user_id,
+      park_id,
+      visit_date,
+      passengers,
+    });
+    res.status(201).json({
+      data: response,
+      success: true,
+      message: "Successfully created the booking",
+      error: {},
+    });
+  } catch (error) {
+    console.log("Something went wrong in the controller layer");
+    res.status(500).json({
+      data: {},
+      success: false,
+      message: "Successfully created the booking",
+      error: error,
+    });
+  }
+};
+
+module.exports = createBooking;
